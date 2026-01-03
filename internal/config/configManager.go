4ff05b4a646f429bc53c5007288a3ef7fb38d376
andreas.oentoro <oentoro.andreas@gmail.com>
2026-01-04 06:45:55 +0800

    Add Config Read Function

---
+++ internal/config/configManager.go
@@ -0,0 +1,77 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
+const configFileName = ".gatorconfig.json"
+
+type Config struct {
+	DbUrl       string `json:"db_url"`
+	CurrentUser string `json:"current_user_name"`
+}
+
+func Read() (Config, error) {
+	configFilePath, err := getConfigFilePath()
+	data, err := os.ReadFile(configFilePath)
+	if err != nil {
+		log.Printf("cannot unmarshal config file: %v", err)
+		return Config{}, err
+	}
+
+	var config Config
+	if err := json.Unmarshal(data, &config); err != nil {
+		log.Printf("cannot unmarshal config file: %v", err)
+		return Config{}, err
+	}
+
+	return config, nil
+}
+
+func (c *Config) SetUser(userName string) error {
+	c.CurrentUser = userName
+	if err := write(*c); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getConfigFilePath() (string, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	configFilePath := homeDir + "/" + configFileName
+
+	return configFilePath, nil
+}
+
+/**
+* 0644
+* │││
+* ││└─ Others (everyone else):  4 = read only
+* │└── Group:                   4 = read only
+* └─── Owner:                   6 = read + write
+ */
+func write(cfg Config) error {
+	configFilePath, err := getConfigFilePath()
+	if err != nil {
+		return fmt.Errorf("cannot set current user: %v", err)
+	}
+
+	data, err := json.MarshalIndent(cfg, "", "  ")
+	if err != nil {
+		return fmt.Errorf("cannot marshal config: %v", err)
+	}
+
+	if err := os.WriteFile(configFilePath, data, 0644); err != nil {
+		return fmt.Errorf("cannot write config: %v", err)
+	}
+
+	return nil
+}
