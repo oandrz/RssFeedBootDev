a6c8f14a87af2e4b58609d82ab24feb2c0030bac
andreas.oentoro <oentoro.andreas@gmail.com>
2026-01-05 07:01:58 +0800

    Add Database Config

--- command.go
+++ command.go
@@ -3,12 +3,7 @@ package main
 import (
 	"bootDevGoRss/internal/config"
 	"bootDevGoRss/internal/database"
-	"context"
-	"errors"
 	"fmt"
-	"time"
-
-	"github.com/google/uuid"
 )
 
 type state struct {
@@ -28,78 +23,6 @@ type command struct {
 type commands struct {
 	// This will be a map of command names to their handler functions.
 	mapper map[string]func(*state, command) error
-}
-
-func handlerLogin(state *state, cmd command) error {
-	if len(cmd.args) == 0 {
-		return errors.New("username argument is required")
-	}
-
-	_, err := state.dbQueriesData.GetUser(context.Background(), cmd.args[0])
-	if err != nil {
-		return fmt.Errorf("user %s does not exists", cmd.args[0])
-	}
-
-	if err := state.configData.SetUser(cmd.args[0]); err != nil {
-		return err
-	}
-
-	fmt.Println("Logged in as", cmd.args[0])
-	return nil
-}
-
-func handlerRegister(state *state, cmd command) error {
-	if len(cmd.args) == 0 {
-		return errors.New("username argument is required")
-	}
-
-	_, err := state.dbQueriesData.GetUser(context.Background(), cmd.args[0])
-	if err == nil {
-		return fmt.Errorf("user %s already exists", cmd.args[0])
-	}
-
-	user, err := state.dbQueriesData.CreateUser(context.Background(), database.CreateUserParams{
-		ID:        uuid.New(),
-		CreatedAt: time.Now(),
-		UpdatedAt: time.Now(),
-		Name:      cmd.args[0],
-	})
-	if err != nil {
-		return fmt.Errorf("cannot create user: %v", err)
-	}
-
-	if err := state.configData.SetUser(cmd.args[0]); err != nil {
-		return err
-	}
-
-	fmt.Printf("User success fully created and registered:\n id: %v, name: %v\n", user.ID, user.Name)
-	return nil
-}
-
-func handlerDelete(state *state, cmd command) error {
-	err := state.dbQueriesData.DeleteUsers(context.Background())
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func handlerGetUsers(state *state, cmd command) error {
-	users, err := state.dbQueriesData.GetUsers(context.Background())
-	if err != nil {
-		return err
-	}
-
-	for _, user := range users {
-		if user.Name == state.configData.CurrentUser {
-			fmt.Printf("* %s (current)\n", user.Name)
-		} else {
-			fmt.Printf("* %s\n", user.Name)
-		}
-	}
-
-	return nil
 }
 
 /*
