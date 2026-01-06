be55a37c296dda6d3f6ee61c541d8b41512f26ee
andreas.oentoro <oentoro.andreas@gmail.com>
2026-01-07 07:38:22 +0800

    Add Fix Unique Constraint Query

--- sql/schema/002_feeds.sql
+++ sql/schema/002_feeds.sql
@@ -11,10 +11,11 @@ create table feed_follows (
     id uuid primary key,
     created_at timestamp not null,
     updated_at timestamp not null,
-    feed_id uuid unique not null ,
-    user_id uuid unique not null ,
+    feed_id uuid not null,
+    user_id uuid not null,
     foreign key (feed_id) references feeds(id) on delete cascade,
-    foreign key (user_id) references users(id) on delete cascade
+    foreign key (user_id) references users(id) on delete cascade,
+    unique (user_id, feed_id)
 );
 
 -- +goose Down
